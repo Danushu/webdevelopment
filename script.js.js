855b55dbbfa68ca72520a3791d280064b4d9d36c
Danushu <163425620+Danushu@users.noreply.github.com>
2025-01-06 19:20:25 +0530

    code for creating website

---
+++ script.js.js
@@ -0,0 +1,9 @@
+// Function to handle adding a product to the cart
+function addToCart(productName) {
+  alert(productName + ' added to cart!');
+}
+
+// Function to handle buying a product immediately
+function buyNow(productName) {
+  alert('You are buying ' + productName + ' now!');
+}
